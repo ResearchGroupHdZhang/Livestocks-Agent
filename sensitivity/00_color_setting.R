fffa7691c54b9f9445bed090ae14d3d8ad3c524c
Yanis Ye <120561403+YanisYe@users.noreply.github.com>
2025-05-21 17:54:33 +0800

    Add files via upload

---
+++ sensitivity/00_color_setting.R
@@ -0,0 +1,20 @@
+
+colorgroup1 <- 
+  c(
+    "#b1a130",
+    "#5fbcc3",
+    "#57b84c",
+    "#7699f9",
+    "#e269dd",
+    "#740b7c"
+  )
+
+colorgroup2 <- 
+  c(
+    "#ebe8c3",
+    "#cbeef0",
+    "#caecd0",
+    "#dbe5fd",
+    "#f6d9f6",
+    "#d9c0dd"
+  )
